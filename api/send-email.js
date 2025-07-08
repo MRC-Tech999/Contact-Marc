17a7bdbe741a446f80f2ba4d56cb27b3df30ee8d
Marchech <emprerorsukuna@gmail.com>
2025-07-08 09:12:17 -0400

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,35 +1,34 @@
-require('dotenv').config();
+// api/send-email.js
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email, phone, subject, message } = req.body;
 
-    // Création du transporteur avec Gmail
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER,  // Ton adresse email
+        user: process.env.EMAIL_USER,  // Ton adresse e-mail
         pass: process.env.EMAIL_PASS   // Le mot de passe d'application
       }
     });
 
     const mailOptions = {
       from: email,
-      to: 'emprerorsukuna@gmail.com',  // Email où tu veux recevoir le message
+      to: 'ton-email@example.com',  // Ton adresse e-mail pour recevoir les messages
       subject: `Nouveau message de ${name}: ${subject}`,
       text: `Nom: ${name}\nEmail: ${email}\nNuméro de téléphone: ${phone}\nSujet: ${subject}\nMessage: ${message}`
     };
 
     try {
-      // Envoi de l'email
       await transporter.sendMail(mailOptions);
       return res.status(200).json({ message: 'Email envoyé avec succès!' });
     } catch (error) {
-      console.log(error);  // Afficher l'erreur dans les logs
+      console.log(error);
       return res.status(500).json({ message: 'Erreur lors de l\'envoi de l\'email', error });
     }
   } else {
     return res.status(405).json({ message: 'Méthode non autorisée' });
   }
-}
+};
