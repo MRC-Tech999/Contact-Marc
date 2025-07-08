71aebc26b389a33904db4e8ad48efc7c62a9ccfb
Marchech <emprerorsukuna@gmail.com>
2025-07-08 09:04:41 -0400

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,25 +1,24 @@
-require('dotenv').config(); // Charger les variables d'environnement
-
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email, phone, subject, message } = req.body;
 
-    // Crée un transporteur avec les données Gmail
+    // Création du transporteur avec Gmail
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER, // Ton adresse e-mail
-        pass: process.env.EMAIL_PASS  // Le mot de passe d'application Gmail
+        user: process.env.EMAIL_USER,  // Ton adresse email
+        pass: process.env.EMAIL_PASS   // Le mot de passe d'application
       }
     });
 
     const mailOptions = {
       from: email,
-      to: 'ton-email@example.com',  // L'email où tu veux recevoir les messages
+      to: 'emprerorsukuna@gmail.com',  // Email où tu veux recevoir le message
       subject: `Nouveau message de ${name}: ${subject}`,
-      text: `Nom: ${name}\nEmail: ${email}\nTéléphone: ${phone}\nSujet: ${subject}\nMessage: ${message}`
+      text: `Nom: ${name}\nEmail: ${email}\nNuméro de téléphone: ${phone}\nSujet: ${subject}\nMessage: ${message}`
     };
 
     try {
@@ -27,6 +26,7 @@ export default async function handler(req, res) {
       await transporter.sendMail(mailOptions);
       return res.status(200).json({ message: 'Email envoyé avec succès!' });
     } catch (error) {
+      console.log(error);  // Afficher l'erreur dans les logs
       return res.status(500).json({ message: 'Erreur lors de l\'envoi de l\'email', error });
     }
   } else {
